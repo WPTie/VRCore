427a515d3f3bdb00b573feca06e1daf02565de1e
Ahmad Awais <mrahmadawais@gmail.com>
2016-03-10 16:05:19 +0500

    vr_get_rental_by_key 💯

--- assets/rental/methods-get-rental.php
+++ assets/rental/methods-get-rental.php
@@ -19,7 +19,17 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 1.0.0
  */
 if ( ! function_exists( 'vr_get_rental_obj' ) ) {
-	function vr_get_rental_obj( $the_rental_ID ) {
-		return new VR_Get_Rental( $the_rental_ID );
+	function vr_get_rental_by_key( $the_rental_ID, $the_rental_key ) {
+		// Bails if no ID.
+		if ( ! $the_rental_ID ){
+			return 'No Rental ID provided!';
+		}
+
+		// Bails if no key.
+		if ( ! $the_rental_key ) {
+			return 'No key provided!';
+		}
+
+		return new VR_Get_Rental( $the_rental_ID )->get_rental_meta( $this->meta_keys[ $the_rental_key ] );
 	}
 }
