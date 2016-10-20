8a86b7e7e5d5f1489488a6c8ab695faeebff0614
Ahmad Awais <mrahmadawais@gmail.com>
2016-10-20 05:36:47 +0500

    FIX max_file_uploads 1 💯

--- assets/template/rental/favorite/class-vr-rental-favorite-meta.php
+++ assets/template/rental/favorite/class-vr-rental-favorite-meta.php
@@ -72,6 +72,7 @@ class VR_Rental_Favorite_Meta {
 					'type'             => 'image_advanced',
 					'name'             => __('Header Background Image', 'VRC'),
 					'desc'             => __( 'Consult the documentation for required image size.', 'VRC' ),
+					'max_file_uploads' => 1,
 	            ),
 				// Summary.
 				array(
