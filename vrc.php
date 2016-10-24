ea553e3bc9d5d9c0f8775724bdc4275c31405213
Ahmad Awais <mrahmadawais@gmail.com>
2016-10-24 20:35:58 +0500

    NEW: Version 1.0.0 💯

--- vrc.php
+++ vrc.php
@@ -6,7 +6,7 @@
  * Author: mrahmadawais, WPTie
  * Author URI: http://WPTie.com/
  * Text Domain: VR
- * Version: 0.0.9
+ * Version: 1.0.0
  * License: GPL v2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
